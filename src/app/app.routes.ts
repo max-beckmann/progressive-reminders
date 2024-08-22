278afa7909c8ea39738436ff18327eb39183c5f2
Maximilian Beckmann <beckmann.max@protonmail.com>
2024-08-22 18:27:05 +0200

    start styling main overview page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { NewPageComponent } from './components/pages/new/new-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'new', component: NewPageComponent }
+];
 
