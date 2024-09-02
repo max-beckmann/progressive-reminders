b1328bae2771303f4ea0bac637635acf3e140ad7
Maximilian Beckmann <beckmann.max@protonmail.com>
2024-09-02 18:56:27 +0200

    add name input for new list page

--- src/app/components/pages/new-list-page/new-list-page.component.ts
+++ src/app/components/pages/new-list-page/new-list-page.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
+import { ContainerComponent } from '../../container/container.component';
+import { IconComponent, IconType } from '../../icon/icon.component';
 
 @Component({
   selector: 'app-new-list-page',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    ContainerComponent,
+    IconComponent
   ],
   templateUrl: './new-list-page.component.html',
   styleUrl: './new-list-page.component.scss'
 })
 export class NewListPageComponent {
+  protected readonly IconType = IconType;
 }
