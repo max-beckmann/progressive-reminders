05c445a2ce0805aee9fc660cdde94cbdd2bb8853
Maximilian Beckmann <beckmann.max@protonmail.com>
2024-09-02 21:10:53 +0200

    add color picker

--- src/app/components/pages/new-list-page/new-list-page.component.ts
+++ src/app/components/pages/new-list-page/new-list-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
 import { ContainerComponent } from '../../container/container.component';
 import { IconComponent, IconType } from '../../icon/icon.component';
@@ -15,5 +15,22 @@ import { IconComponent, IconType } from '../../icon/icon.component';
   styleUrl: './new-list-page.component.scss'
 })
 export class NewListPageComponent {
+  color = signal<string>('red');
+  colorOptions = ['#FE3C30', '#FE9500', '#FECC02', '#19C759', '#51AAF2', '#007AFF', '#5756D5', '#EA426A', '#BF77DB', '#9D8563', '#5B6670', '#D9A69E'];
+
+  constructor() {
+    effect(() => {
+      document.documentElement.style.setProperty('--new-list-color', this.color());
+    });
+  }
+
+  pickColor(index: number): void {
+    document.querySelectorAll('.color-option').forEach((option, i) => {
+      i == index ? option.classList.add('selected') : option.classList.remove('selected');
+    });
+
+    this.color.set(this.colorOptions[index])
+  }
+
   protected readonly IconType = IconType;
 }
