75dcde40ce88faf63bf49b8daf9b24dce666d46d
Maximilian Beckmann <beckmann.max@protonmail.com>
2024-09-07 14:06:26 +0200

    get list count from database

--- src/app/components/aggregate-items/inline-list/inline-list.component.ts
+++ src/app/components/aggregate-items/inline-list/inline-list.component.ts
@@ -1,37 +1,49 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { Item, List } from '../../../../../model';
 import { AggregateItemComponent } from '../aggregate-item.component';
 import { RouterLink } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { database } from '../../../../../database';
 
 @Component({
   selector: 'app-inline-list',
   standalone: true,
   imports: [
     AggregateItemComponent,
-    RouterLink
+    RouterLink,
+    AsyncPipe
   ],
   templateUrl: './inline-list.component.html',
   styleUrl: './inline-list.component.scss'
 })
 export class InlineListComponent {
   list = input.required<List>();
-  count = input<number>();
+  count = signal<number>(0);
 
   get link(): string {
     return `/list/${this.list().id}`
   }
 
-  get aggregateItem(): Item {
+  aggregateItem = computed<Item>(() => {
     const { title, icon, color } = this.list();
 
     return {
       title,
-      subtitle: '0',
+      subtitle: this.count().toString(),
       icon: {
         type: icon,
         backgroundColor: color
       },
       hasArrow: true
     }
+  });
+
+  constructor() {
+    void this.initCount();
+  }
+
+  private async initCount() {
+    const count = await database.reminders.where('associatedList').equals(this.list().id!).count()
+    this.count.set(count);
   }
 }
